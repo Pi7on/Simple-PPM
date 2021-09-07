4f6f1ed26562c8b1dd9228565201d049ea239ff3
Pi7on <ramenmantheoriginal@gmail.com>
2021-09-07 16:09:21 +0200

    remove old lerp_double()

--- src/helpers.c
+++ src/helpers.c
@@ -1,8 +1,1 @@
 #include "helpers.h"
-
-double lerp_double(const double a, const double b, const double weight) {
-    if (a == b) {
-        return a;
-    }
-    return ((a * (1.0f - weight)) + (b * weight));
-}
--- src/helpers.h
+++ src/helpers.h
@@ -7,7 +7,4 @@
 // linear interpolation
 #define LERP(a, b, weight) ((a) == (b) ? (a) : ((a) * (1.0f - (weight)) + ((b) * (weight))))
 
-// linear interpolation between two doubles
-double lerp_double(const double a, const double b, const double weight);
-
 #endif /* HELPERS_H */
