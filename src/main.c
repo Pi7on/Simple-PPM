a30ff60915eb1de9443c5490863ab5577e031981
Pi7on <ramenmantheoriginal@gmail.com>
2022-01-21 16:22:07 +0100

    update main

--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 #include "dither.h"
+#include "grayscale.h"
 #include "ppm.h"
+#include "rotate.h"
 #include "stdio.h"
 
 #define DATAPATH "..\\..\\data\\"
 
 int main() {
     PPMImage *input = PPMImage_read(DATAPATH "2b.ppm");
-    PPMImage *output = PPM_filter_FSDither(input, 1);
-    PPMImage_write(DATAPATH "2b_dither.ppm", output);
+    imgGrayscale(input);
+    PPMImage *output = PPMImage_transform_rotate90(input, 2);
+    PPMImage_write(DATAPATH "2b_out.ppm", PPM_filter_FSDither(output, 2));
     return 0;
 }
