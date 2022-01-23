348bbbcbb388d99df0a8acc921a92d9651048e6c
Pi7on <ramenmantheoriginal@gmail.com>
2022-01-23 22:01:57 +0100

    avoid redeclaring scaling constants for each function call

--- src/grayscale.c
+++ src/grayscale.c
@@ -1,10 +1,10 @@
 #include "grayscale.h"
 
-void pixelGrayscale(PPMImage *input, unsigned int x, unsigned int y){
-    const float r_scale = 0.299;
-    const float g_scale = 0.587;
-    const float b_scale = 0.114;
+static const float r_scale = 0.299f;
+static const float g_scale = 0.587f;
+static const float b_scale = 0.114f;
 
+void pixelGrayscale(PPMImage *input, unsigned int x, unsigned int y) {
     PPMPixel pixel = input->data[x + ((input->w) * y)];
     pixel.chan.r = input->data[x + ((input->w) * y)].chan.r * r_scale + input->data[x + ((input->w) * y)].chan.g * g_scale + input->data[x + ((input->w) * y)].chan.b * b_scale;
     pixel.chan.g = input->data[x + ((input->w) * y)].chan.r * r_scale + input->data[x + ((input->w) * y)].chan.g * g_scale + input->data[x + ((input->w) * y)].chan.b * b_scale;
@@ -12,10 +12,10 @@ void pixelGrayscale(PPMImage *input, unsigned int x, unsigned int y){
     input->data[x + ((input->w) * y)] = pixel;
 }
 
-void imgGrayscale(PPMImage *input){
+void imgGrayscale(PPMImage *input) {
     for (unsigned int y = 0; y < input->h; y++) {
         for (unsigned int x = 0; x < input->w; x++) {
-            pixelGrayscale(input,x,y);
+            pixelGrayscale(input, x, y);
         }
     }
 }
